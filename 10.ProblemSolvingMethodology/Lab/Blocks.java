548985098bf47f4b285099b5d0945bba8f9a1274
Atanas <naskoraist@gmail.com>
2018-04-28 16:30:34 +0300

    Problem-solving methodology lab solutions

---
+++ 10.ProblemSolvingMethodology/Lab/Blocks.java
@@ -0,0 +1,60 @@
+package ProblemSolvingMethodology.Lab;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Blocks {
+
+    private static Set<String> variations = new TreeSet<>();
+    private static Set<String> rotations = new HashSet<>();
+    private static char[] elements;
+    private static char[] block = new char[4];
+    private static Set<Character> used = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        elements = new char[n];
+        for (int i = 0; i < n; i++) {
+            elements[i] = (char)(i + 'A');
+        }
+        generateRecursive(0);
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Number of blocks: %d%n", variations.size()));
+        for (String variation : variations) {
+            sb.append(variation).append(System.lineSeparator());
+        }
+        System.out.print(sb);
+    }
+    private static void generateRecursive(int index) {
+        if (index >= 4) {
+            if (!rotations.contains(String.valueOf(block))) {
+                variations.add(String.valueOf(block));
+                addAllRotations();
+            }
+            return;
+        }
+        for (int i = 0; i < elements.length; i++) {
+            char element = elements[i];
+            if (!used.contains(element)) {
+                used.add(element);
+                block[index] = element;
+                generateRecursive(index + 1);
+                used.remove(element);
+            }
+        }
+    }
+
+    private static void addAllRotations() {
+        for (int i = 0; i < 4; i++) {
+            char temp = block[0];
+            block[0] = block[1];
+            block[1] = block[2];
+            block[2] = block[3];
+            block[3] = temp;
+            rotations.add(String.valueOf(block));
+        }
+    }
+}
---
+++ 10.ProblemSolvingMethodology/Lab/ZigZagMatrix.java
@@ -0,0 +1,149 @@
+package ProblemSolvingMethodology.Lab;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ZigZagMatrix {
+
+    // 25/100 in Judge but works correctly as opposed to the author solution
+
+    private static int[][] matrix;
+    private static int[][] evenUpMatrix;
+    private static int[][] evenDownMatrix;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int rows = Integer.parseInt(reader.readLine());
+        int cols = Integer.parseInt(reader.readLine());
+        matrix = new int[rows][cols];
+        evenUpMatrix = new int[rows][cols];
+        evenDownMatrix = new int[rows][cols];
+        fillOriginalMatrix(reader, rows, cols);
+        fillMemoMatrices(rows, cols);
+
+        int maxSum = Integer.MIN_VALUE;
+        int maxSumRow = -1;
+        boolean evenUp = false;
+        for (int row = 0; row < rows; row++) {
+            int current = evenUpMatrix[row][cols - 1];
+            if (current > maxSum) {
+                evenUp = true;
+                maxSum = current;
+                maxSumRow = row;
+            }
+            current = evenDownMatrix[row][cols - 1];
+            if (current >= maxSum) {
+                evenUp = false;
+                maxSum = current;
+                maxSumRow = row;
+            }
+        }
+
+        Deque<Integer> path = buildPath(rows, cols, maxSumRow, evenUp);
+        System.out.println(getOutputString(maxSum, path));
+    }
+
+    private static Deque<Integer> buildPath(int rows, int cols, int maxSumRow, boolean evenUp) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(matrix[maxSumRow][cols - 1]);
+        int[][] maxSumMatrix = evenUp ? evenUpMatrix : evenDownMatrix;
+        for (int col = cols - 2; col >= 0; col--) {
+            int colMax = Integer.MIN_VALUE;
+            boolean checkUp = evenUp ? col % 2 == 0 : col % 2 == 1;
+            if (checkUp) {
+                for (int row = maxSumRow - 1; row >= 0; row--) {
+                    int current = maxSumMatrix[row][col];
+                    if (current >= colMax) {
+                        colMax = current;
+                        maxSumRow = row;
+                    }
+                }
+            } else {
+                for (int row = maxSumRow + 1; row < rows; row++) {
+                    int current = maxSumMatrix[row][col];
+                    if (current > colMax) {
+                        colMax = current;
+                        maxSumRow = row;
+                    }
+                }
+            }
+            stack.push(matrix[maxSumRow][col]);
+        }
+        return stack;
+    }
+
+    private static String getOutputString(int maxSum, Deque<Integer> path) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(maxSum).append(" = ");
+        while (!path.isEmpty()) {
+            sb.append(path.pop()).append(" + ");
+        }
+        sb.delete(sb.length() - 3, sb.length());
+        return sb.toString();
+    }
+
+    private static void fillMemoMatrices(int rows, int cols) {
+        for (int row = 0; row < rows; row++) {
+            evenUpMatrix[row][0] = matrix[row][0];
+            evenDownMatrix[row][0] = matrix[row][0];
+        }
+        for (int col = 1; col < cols; col++) {
+            for (int row = 0; row < rows; row++) {
+                int prevMaxDown = findPrevMax(col - 1, row, false);
+                int prevMaxUp = findPrevMax(col - 1, row, true);
+                if (col % 2 == 1) {
+                    evenUpMatrix[row][col] = matrix[row][col] + prevMaxUp;
+                    evenDownMatrix[row][col] = matrix[row][col] + prevMaxDown;
+                } else {
+                    evenUpMatrix[row][col] = matrix[row][col] + prevMaxDown;
+                    evenDownMatrix[row][col] = matrix[row][col] + prevMaxUp;
+                }
+            }
+        }
+    }
+
+    private static void fillOriginalMatrix(BufferedReader reader, int rows, int cols) throws IOException {
+        for (int row = 0; row < rows; row++) {
+            String[] values = reader.readLine().split(",");
+            for (int col = 0; col < cols; col++) {
+                matrix[row][col] = Integer.parseInt(values[col]);
+            }
+        }
+    }
+
+    private static int findPrevMax(int col, int currentRow, boolean checkUp) {
+        int max = 0;
+        if (checkUp) {
+            for (int row = 0; row < currentRow; row++) {
+                int current;
+                if (col % 2 == 1) {
+                    current = evenDownMatrix[row][col];
+                } else {
+                    current = evenUpMatrix[row][col];
+                }
+                if (current > max) {
+                    max = current;
+                }
+            }
+        } else {
+            for (int row = currentRow + 1; row < matrix.length; row++) {
+                int current;
+                if (col % 2 == 1) {
+                    current = evenUpMatrix[row][col];
+                } else {
+                    current = evenDownMatrix[row][col];
+                }
+                if (current > max) {
+                    max = current;
+                }
+            }
+        }
+        return max;
+    }
+}
+
+
+
